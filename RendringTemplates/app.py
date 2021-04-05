8f9917be55c7db95f0cc3ad9ee0f500d25870751
Akash Kumar <akashrajput2012001@gmail.com>
2021-04-06 01:28:48 +0530

    Crud

--- RendringTemplates/app.py
+++ RendringTemplates/app.py
@@ -1,9 +1,9 @@
 
-from flask import Flask,render_template
+from operator import pos
+from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import default_exceptions
 from datetime import datetime
-
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///posts.db'
@@ -21,12 +21,40 @@ class BlogPost(db.Model):
         return 'Blog Post'+str(self.id)
 
 
+all_posts=[]
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/posts',methods=['GET','POST'])
 def post():
+    if request.method=='POST':
+        post_title=request.form['title']
+        post_content=request.form['content']
+        post_author=request.form['author']
+        new_post=BlogPost(title=post_title,content=post_content,author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts=BlogPost.query.order_by(BlogPost.post_date).all()  
     return render_template('post.html',posts=all_posts)
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post=BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=["GET","POST"])
+def edit(id):
+    post=BlogPost.query.get_or_404(id)
+    post.title=request.form['title']
+    post.content=request.form['content']
+    post.author=request.form['author']
+  
+    db.session.commit()
+    return redirect('/posts')
+
 if __name__=="__main__":
     app.run(debug=True)
